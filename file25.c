edda7fc8d713000b0a8a3c79758cc5569daa7411
Anudeep0517 <komatianudeep@gmail.com>
2026-02-18 12:06:45 +0530

    Update file25.c

--- file25.c
+++ file25.c
@@ -2,30 +2,43 @@
 #include <fcntl.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <stdlib.h>
 
 int main() {
-    int file = 0;
-
-    if ((file = open("test file.txt", O_RDONLY)) < 0)
+    int file;
+    file = open("test file.txt", O_RDONLY);
+    if (file < 0) {
+        perror("Error opening file");
         return 1;
+    }
+    char buffer[20];
+    ssize_t bytesRead;
 
-    char buffer[20];   
-    if (read(file, buffer, 19) != 19)
+    bytesRead = read(file, buffer, 19);
+    if (bytesRead < 0) {
+        perror("Error reading file");
+        close(file);
         return 1;
+    }
+    buffer[bytesRead] = '\0';
+    printf("First read: %s\n", buffer);
 
-    buffer[19] = '\0';   
-    printf("%s\n", buffer);
-
-    if (lseek(file, 10, SEEK_SET) < 0)
+    if (lseek(file, 10, SEEK_SET) < 0) {
+        perror("Error with lseek");
+        close(file);
         return 1;
+    }
 
-    if (read(file, buffer, 19) != 19)
+    bytesRead = read(file, buffer, 19);
+    if (bytesRead < 0) {
+        perror("Error reading file");
+        close(file);
         return 1;
+    }
 
-    buffer[19] = '\0';
-    printf("%s\n", buffer);
+    buffer[bytesRead] = '\0';
+    printf("Second read: %s\n", buffer);
 
     close(file);
     return 0;
 }
-
